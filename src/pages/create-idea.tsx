ebd50341bb5002da471cca8534a2580d4399874f
TalentEdge-Khoa Le <khoa.le@talentedge.ai>
2025-01-21 09:14:49 +0000

    Sync with upstream, excluding workflows

--- src/pages/create-idea.tsx
+++ src/pages/create-idea.tsx
@@ -1,23 +1,37 @@
 import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { CONFIG } from 'src/config-global';
 import { DashboardContent } from 'src/layouts/dashboard';
 
 import { Button, Card, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+import { Icon } from '@iconify/react';
+import { PillarSelect } from 'src/sections/pillar/pillar-select';
 
 // ----------------------------------------------------------------------
 
 export default function Page() {
-  const [inputType, setInputType] = useState(true);
+  const [isRecording, setIsRecording] = useState(false);
+  const [pillar, setPillar] = useState('pillar-id-1');
+  const [pillarList, setPillarList] = useState([
+    { value: 'pillar-id-1', label: 'Pillar 1' },
+    { value: 'pillar-id-2', label: 'Pillar 2' },
+    { value: 'pillar-id-3', label: 'Pillar 3' },
+    { value: 'pillar-id-4', label: 'Pillar 4' },
+  ]);
 
-  const handleTypeClick = () => {
-    setInputType(true);
-  };
+  const handleRecording = () => {
+    setIsRecording(!isRecording);
+    // TODO: Implement voice recording
+  }
 
-  const handleVoiceClick = () => {
-    setInputType(false);
-  };
+  const handleSort = useCallback((newPillar: string) => {
+    setPillar(newPillar);
+  }, []);
+
+  const handleSubmit = () => {
+    // TODO: Implement submit
+  }
 
   return (
     <>
@@ -31,33 +45,29 @@ export default function Page() {
             Share Your Idea
           </Typography>
 
+          {/* Select content pillar */}
+          <PillarSelect pillarName={pillar}
+            onSort={handleSort}
+            options={pillarList}/>
+
           {/* Buttons to choose Type or Voice input */}
-          <Box display="flex" alignItems="center" mb={5} sx={{ width: 'auto' }}>
+          <Box display="flex" alignItems="center" my='1rem' sx={{ width: 'auto' }} gap='1rem'>
             <Button 
-              variant={ inputType ? 'contained' : 'outlined' }
-              color='inherit' 
-              size='large' 
-              sx={{ width: '50%', mr: '0.5rem' }}
-              onClick={handleTypeClick}
-            >Type</Button>
-            <Button 
-              variant={ inputType ? 'outlined' : 'contained' } 
-              color='inherit' 
-              size='large' 
-              sx={{ width: '50%', ml: '0.5rem' }}
-              onClick={handleVoiceClick}
-            >Voice</Button>
+              sx={{ padding: '2rem', width: '20%' }}
+              color={isRecording ? 'error' : 'inherit'}
+              size='large'
+              startIcon={<Icon width='2rem'
+                icon={isRecording ? 'fluent:mic-record-24-filled' : 'ph:microphone-fill'}/>} 
+              onClick={handleRecording}
+            >
+              {isRecording ? 'Stop Recording' : 'Start Recording'}
+            </Button>
+
+            {/* TODO: Replace by text editor */}
+            <Card sx={{ padding: '2rem', width: '80%', height: '300px' }}>
+              Text editor placeholder
+            </Card>
           </Box>
-
-          <Card sx={{ padding: '2rem', mb: '1rem' }}
-            style={{ display: inputType ? 'none' : 'block' }}  
-          >
-            Voice recording placeholder
-          </Card>
-
-          <Card sx={{ padding: '2rem' }}>
-            Text editor placeholder
-          </Card>
 
           {/* Submit button */}
           <Button
@@ -65,6 +75,7 @@ export default function Page() {
             color='primary' 
             size='large' 
             sx={{ width: '100%', mt: '2rem' }}
+            onClick={handleSubmit}
           >Generate content</Button>
         </Card>
       </DashboardContent>
